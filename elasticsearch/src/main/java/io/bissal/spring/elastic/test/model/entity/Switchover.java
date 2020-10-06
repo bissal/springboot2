45c1c5dc97d45abb9c48936aa8895138daf527fe
곽옥동 <gwak.okdong@ucomp.co.kr>
2020-10-06 15:24:37 +0900

    add audit column, add findLastEntry

--- elasticsearch/src/main/java/io/bissal/spring/elastic/test/model/entity/Switchover.java
+++ elasticsearch/src/main/java/io/bissal/spring/elastic/test/model/entity/Switchover.java
@@ -1,36 +1,48 @@
 package io.bissal.spring.elastic.test.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Entity
 @Getter
 @NoArgsConstructor
-@Entity
+@AllArgsConstructor
+@Builder
+@ToString
+@EntityListeners(AuditingEntityListener.class)
 public class Switchover {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Boolean enabled;
+    private Boolean isAliveMain;
+    private Boolean isAliveDr;
 
     @CreatedDate
     private LocalDateTime createdDate;
-    @LastModifiedDate
-    private LocalDateTime lastModifiedDate;
-
     @CreatedBy
     private String createdBy;
+    private String createdFrom;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
     @LastModifiedBy
     private String lastModifiedBy;
+    private String lastModifiedFrom;
 }
